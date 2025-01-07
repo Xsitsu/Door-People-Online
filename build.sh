7a8fa42b4f6a451c28301898e0d742e435fb47c8
Xsitsu <jacoblocke21@live.com>
2025-01-06 16:58:12 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ build_dir="$script_dir"/cmake-build
 
 cmake -B "$build_dir" -S "$script_dir"
 
-cmake --build "$build_dir" --target freetype
 cmake --build "$build_dir" --target Server
 cmake --build "$build_dir" --target Client
